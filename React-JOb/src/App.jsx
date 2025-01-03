a9f5e66fe7eb583a8978fb306f83e64c08041e5d
mehrab-shakib <fatscorpieeth@gmail.com>
2025-01-03 12:28:33 +0600

    Added Route, Homepage Routing Done

--- React-JOb/src/App.jsx
+++ React-JOb/src/App.jsx
@@ -1,19 +1,21 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
-const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layout/MainLayout";
 
-    </>
-  );
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
+const App = () => {
+  return <RouterProvider router={router} />;
 };
 
 export default App;
